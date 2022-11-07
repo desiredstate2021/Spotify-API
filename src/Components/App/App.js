58e67284ab0779c4abe3edc0335dca420ccc0fd6
unknown <desiredshellstate@gmail.com>
2022-11-07 09:43:32 +0100

    Love React

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span class="highlight">mmm</span>ing</h1>
+      <div class="App">
+
+        <div class="App-playlist">
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
